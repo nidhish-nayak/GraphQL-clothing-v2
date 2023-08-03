39acbb62fc096c2a80da9c0286114236dc9fc066
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-08-04 01:32:07 +0530

    Updated 2 files to fix warnings on yarn start

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,20 @@
 import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { UserContext } from '../../contexts/user.context';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import {
-  NavigationContainer,
-  NavLinks,
-  NavLink,
   LogoContainer,
+  NavLink,
+  NavLinks,
+  NavigationContainer,
 } from './navigation.styles';
 
 const Navigation = () => {
