888304d59a55e754263918d7880387ff593abc69
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-19 22:51:05 +0530

    Fetching collections data successfull

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -24,9 +24,20 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log(data)
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
 
-  const value = { categoriesMap };
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data])
+
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
