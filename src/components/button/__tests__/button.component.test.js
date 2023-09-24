ac988963750ed11667f84b27c3a911b0918dec83
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-25 00:25:37 +0530

    Added test for google button

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Button from "../button.component.jsx";
+import Button, { BUTTON_TYPE_CLASSES } from "../button.component.jsx";
 
 describe("button tests", () => {
 	test("should render base button when nothing is passed", () => {
@@ -7,5 +7,12 @@ describe("button tests", () => {
 
 		const buttonElement = screen.getByText(/test/i);
 		expect(buttonElement).toHaveStyle("background-color: black");
+	});
+
+	test("should render google button when google button type passed", () => {
+		render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />);
+
+		const googleButtonElement = screen.getByRole("button");
+		expect(googleButtonElement).toHaveStyle("background-color: #4285f4");
 	});
 });
